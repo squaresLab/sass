fef1aff88a772860a9b1076d3e03582d7ef053fa
Cody Kinneer <ckinneer@cs.cmu.edu>
2016-11-03 11:52:28 -0400

    back to single obj case

--- src/main/java/ecj/MutationBuilder.java
+++ src/main/java/ecj/MutationBuilder.java
@@ -122,12 +122,13 @@ public class MutationBuilder extends ec.gp.GPNodeBuilder {
     }
 	
 	  protected static GPIndividual loadStartInd(EvolutionState state) {
-		  /*
+		  
 		  String indString = 
 					"Evaluated: F\n"+
 					"Fitness: d4554802393809453312|3.3430128051926966E-4|i0|\n"+
 					"Tree 0:\n";
-		  */
+		  
+		  /*
 		  // for multiobjective case
 		 String indString = 
 				  "Evaluated: F\n"+
@@ -136,7 +137,7 @@ public class MutationBuilder extends ec.gp.GPNodeBuilder {
 				  "Strength: d0|0.0|\n"+
 				  "Distance: d4567423964645623803|0.0022887690524564497|\n"+
 				  "Tree 0:\n";
-		  
+		  */
 		  
 		 // if (mtf.nextBoolean(0.90))
 			 indString += "(; (IncreaseTraffic A) (; (DecreaseDimmer B) (; (DecreaseDimmer B) (DecreaseDimmer B))))";
