b97ee86c0d51125d3b28588b9d4e93cfbd31321f
Cody Kinneer <ckinneer@cs.cmu.edu>
2016-11-04 12:05:06 -0400

    econ poor

--- src/main/java/omnet/components/ServerFactory.java
+++ src/main/java/omnet/components/ServerFactory.java
@@ -10,7 +10,7 @@ public class ServerFactory {
 	
 	public Server getA(){
 		Server a = new Server("A"+numServers[0]++);
-		a.setCost(0.5);
+		a.setCost(5);
 		a.power = 150;
 		a.powerPerNormal = 150.0 / 50.0;
 		return a;
@@ -18,7 +18,7 @@ public class ServerFactory {
 
 	public Server getB(){
 		Server b = new Server("B"+numServers[1]++);
-		b.setCost(0.7);
+		b.setCost(7);
 		b.power = 200;
 		b.powerPerNormal = 200.0 / 130.0;
 		return b;
@@ -26,7 +26,7 @@ public class ServerFactory {
 	
 	public Server getC(){
 		Server c = new Server("C"+numServers[2]++);
-		c.setCost(1);
+		c.setCost(10);
 		c.power = 300;
 		c.powerPerNormal = 300.0 / 150.0;
 		return c;
@@ -34,7 +34,7 @@ public class ServerFactory {
 	
 	public Server getD(){
 		Server d = new Server("D"+numServers[3]++);
-		d.setCost(0.5);
+		d.setCost(5);
 		d.power = 80;
 		d.powerPerNormal = 80.0 / 25.0;
 		d.latency = 60;
