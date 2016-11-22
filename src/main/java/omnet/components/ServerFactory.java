586ad7f39c00789cdc30a20722ddd92deff59807
Cody Kinneer <ckinneer@cs.cmu.edu>
2016-11-22 11:43:33 -0500

    added new server types to server factory

--- src/main/java/omnet/components/ServerFactory.java
+++ src/main/java/omnet/components/ServerFactory.java
@@ -5,7 +5,7 @@ public class ServerFactory {
 	int[] numServers;
 	
 	public ServerFactory(){
-		numServers = new int[4];
+		numServers = new int[16];
 	}
 	
 	public Server getA(){
@@ -41,6 +41,105 @@ public class ServerFactory {
 		return d;
 	}
 	
+	public Server getE(){
+		Server a = new Server("E"+numServers[4]++);
+		a.setCost(.5);
+		a.power = 150;
+		a.powerPerNormal = 150.0 / 50.0;
+		return a;
+	}
+
+	public Server getF(){
+		Server b = new Server("F"+numServers[5]++);
+		b.setCost(.7);
+		b.power = 200;
+		b.powerPerNormal = 200.0 / 130.0;
+		return b;
+	}
+	
+	public Server getG(){
+		Server c = new Server("G"+numServers[6]++);
+		c.setCost(1);
+		c.power = 300;
+		c.powerPerNormal = 300.0 / 150.0;
+		return c;
+	}
+	
+	public Server getH(){
+		Server d = new Server("H"+numServers[7]++);
+		d.setCost(.5);
+		d.power = 80;
+		d.powerPerNormal = 80.0 / 25.0;
+		d.latency = 60;
+		return d;
+	}
+	
+	public Server getI(){
+		Server a = new Server("I"+numServers[8]++);
+		a.setCost(.5);
+		a.power = 150;
+		a.powerPerNormal = 150.0 / 50.0;
+		return a;
+	}
+
+	public Server getJ(){
+		Server b = new Server("J"+numServers[9]++);
+		b.setCost(.7);
+		b.power = 200;
+		b.powerPerNormal = 200.0 / 130.0;
+		return b;
+	}
+	
+	public Server getK(){
+		Server c = new Server("K"+numServers[10]++);
+		c.setCost(1);
+		c.power = 300;
+		c.powerPerNormal = 300.0 / 150.0;
+		return c;
+	}
+	
+	public Server getL(){
+		Server d = new Server("L"+numServers[11]++);
+		d.setCost(.5);
+		d.power = 80;
+		d.powerPerNormal = 80.0 / 25.0;
+		d.latency = 60;
+		return d;
+	}
+	
+	public Server getM(){
+		Server a = new Server("M"+numServers[12]++);
+		a.setCost(.5);
+		a.power = 150;
+		a.powerPerNormal = 150.0 / 50.0;
+		return a;
+	}
+
+	public Server getN(){
+		Server b = new Server("N"+numServers[13]++);
+		b.setCost(.7);
+		b.power = 200;
+		b.powerPerNormal = 200.0 / 130.0;
+		return b;
+	}
+	
+	public Server getO(){
+		Server c = new Server("O"+numServers[14]++);
+		c.setCost(1);
+		c.power = 300;
+		c.powerPerNormal = 300.0 / 150.0;
+		return c;
+	}
+	
+	public Server getP(){
+		Server d = new Server("P"+numServers[15]++);
+		d.setCost(.5);
+		d.power = 80;
+		d.powerPerNormal = 80.0 / 25.0;
+		d.latency = 60;
+		return d;
+	}
+	
 	public int[] getNumServers(){
 		return numServers;
 	}
@@ -53,6 +152,18 @@ public class ServerFactory {
 		case "B": index = 1; break;
 		case "C": index = 2; break;
 		case "D": index = 3; break;
+		case "E": index = 4; break;
+		case "F": index = 5; break;
+		case "G": index = 6; break;
+		case "H": index = 7; break;
+		case "I": index = 8; break;
+		case "J": index = 9; break;
+		case "K": index = 10; break;
+		case "L": index = 11; break;
+		case "M": index = 12; break;
+		case "N": index = 13; break;
+		case "O": index = 14; break;
+		case "P": index = 15; break;
 		}
 		
 		return index;
