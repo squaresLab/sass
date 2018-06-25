0a1bed826c98dbad426943c6d7a61d540a9acdca
Cody Kinneer <ckinneer@cs.cmu.edu>
2018-06-25 18:59:35 -0400

    added class to represent dart system/enviroment configuration

--- src/main/java/dart/Dart.java
+++ src/main/java/dart/Dart.java
@@ -1,5 +1,20 @@
 package dart;
 
-public class Dart {
+import system.Fitness;
+import system.SystemState;
+
+public class Dart extends SystemState {
+
+	@Override
+	public boolean isStateValid() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Fitness calculateInstFitness() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
