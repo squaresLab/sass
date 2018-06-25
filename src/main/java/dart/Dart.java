270cc19c30ab93e2be3dd8dade3678e192faf45f
Cody Kinneer <ckinneer@cs.cmu.edu>
2018-06-25 11:44:35 -0400

    started ecj dart code

---
+++ src/main/java/dart/Dart.java
@@ -0,0 +1,5 @@
+package dart;
+
+public class Dart {
+
+}
---
+++ src/main/java/dart/tactics/IncAlt.java
@@ -0,0 +1,32 @@
+package dart.tactics;
+
+import system.SystemState;
+import tactics.FailableTactic;
+
+public class IncAlt extends FailableTactic {
+
+	@Override
+	public double getFailChance() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void visit(SystemState systemState) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void undo(SystemState systemState) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public long getExecutionTime() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
