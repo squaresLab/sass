f2fe629c339781b1de0edceef228abc8239d72b6
Cody Kinneer <ckinneer@cs.cmu.edu>
2018-07-10 21:41:16 -0400

    added interproc call for java -> c direction

--- src/main/java/dart/Dart.java
+++ src/main/java/dart/Dart.java
@@ -1,20 +1,52 @@
 package dart;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import system.Fitness;
 import system.SystemState;
 
 public class Dart extends SystemState {
+	
+	static final String FITNESS_CMD_STRING = "./runDartFitness";
 
 	@Override
 	public boolean isStateValid() {
-		// TODO Auto-generated method stub
-		return false;
+		// the outside simulator will manage everything
+		return true;
 	}
 
 	@Override
 	public Fitness calculateInstFitness() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		// call the dart fitness simulator for fitness calculation
+		Runtime rt = Runtime.getRuntime();
+		String [] args = {};
+		String cmd = FITNESS_CMD_STRING;
+		for (int i = 0; i < args.length; i++) {
+			cmd += " " + args[i];
+		}
+		
+		String s = "";
+		
+		try {
+			Process pr = rt.exec(cmd);
+			BufferedReader stdInput = new BufferedReader(new 
+				     InputStreamReader(pr.getInputStream()));
+			
+			while ((s = stdInput.readLine()) != null) {
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		Fitness ans = new Fitness();
+		
+		ans.put("Profit", (double) Integer.parseInt(s));
+		
+		return ans;
 	}
 
 }
