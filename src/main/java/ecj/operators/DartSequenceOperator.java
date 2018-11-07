366ba7758e2510e8bdb4355083d6303a41670bdc
Cody Kinneer <ckinneer@cs.cmu.edu>
2018-11-07 12:13:23 -0500

    added sequence operator for dart

---
+++ src/main/java/ecj/operators/DartSequenceOperator.java
@@ -0,0 +1,66 @@
+package ecj.operators;
+
+import ec.EvolutionState;
+import ec.Problem;
+import ec.gp.ADFStack;
+import ec.gp.GPData;
+import ec.gp.GPIndividual;
+import ec.gp.GPNode;
+import ecj.DartStateData;
+import ecj.JavaGenerator;
+import ecj.JavaRep;
+import ecj.StateData;
+
+public class DartSequenceOperator extends GPNode implements JavaGenerator {
+
+	public String toString() { return ";"; }
+
+	public int expectedChildren() { return 2;}
+
+	public void eval(final EvolutionState state,
+			final int thread,
+			final GPData input,
+			final ADFStack stack,
+			final GPIndividual individual,
+			final Problem problem)
+	{
+		//double result;
+
+		//now handling with a runtime exception
+		DartStateData o = (DartStateData)input;
+		
+		children[0].eval(state,thread,input,stack,individual,problem);
+		
+		children[1].eval(state,thread,input,stack,individual,problem);
+		
+//		o.getAllFinalStates(this);
+
+//		children[0].eval(state,thread,input,stack,individual,problem);
+//
+//		//System.out.println("middle: "+sd.toString()+"\n");
+//		children[1].eval(state,thread,input,stack,individual,problem);
+		//System.out.println("after: "+sd.toString()+"\n");
+		//the result information should now be held in input
+		
+
+
+	}
+
+	@Override
+	public JavaRep generateJava(JavaRep java) {
+		((JavaGenerator) children[0]).generateJava(java);
+		java.newLine();
+		((JavaGenerator) children[1]).generateJava(java);
+		java.newLine();
+		return java;
+	}
+
+
+}
+
+
+
+
+
+
+
