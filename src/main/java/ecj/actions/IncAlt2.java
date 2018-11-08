01f1596b0a0e9f6a496ec9f57bd55afba9749a15
Cody Kinneer <ckinneer@cs.cmu.edu>
2018-11-07 22:20:48 -0500

    added ecj tactics for 4 new dart tactics

---
+++ src/main/java/ecj/actions/IncAlt2.java
@@ -0,0 +1,26 @@
+package ecj.actions;
+
+import ec.EvolutionState;
+import ec.Problem;
+import ec.gp.ADFStack;
+import ec.gp.GPData;
+import ec.gp.GPIndividual;
+import ec.gp.GPNode;
+import ecj.DartStateData;
+
+public class IncAlt2 extends GPNode {
+
+	@Override
+	public void eval(EvolutionState arg0, int arg1, GPData arg2, ADFStack arg3, GPIndividual arg4, Problem arg5) {
+		DartStateData o = (DartStateData) arg2;
+		
+		o.plan.getTactics().add(new dart.tactics.IncAlt2());
+		
+	}
+
+	@Override
+	public String toString() {
+		return "IncAlt2";
+	}
+
+}
