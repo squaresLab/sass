7680cfcb20eff034b9e3b94aa15f972589d63a32
Cody Kinneer <ckinneer@cs.cmu.edu>
2019-08-09 15:58:20 -0400

    filled in new Scenario type for formalizing change scenarios in omnet case study

--- src/main/java/omnet/Scenario.java
+++ src/main/java/omnet/Scenario.java
@@ -1,12 +1,14 @@
 package omnet;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import omnet.components.Datacenter;
 import omnet.components.DatacenterFactory;
+import tactics.Tactic;
 
 public class Scenario {
-	
+
 	// omnet constant parameters
 	int SYSTEM_DEMAND = 1000;
 	
@@ -14,7 +16,8 @@ public class Scenario {
 
 	double DIMMED_PROFIT_PER_SECOND = 1;
 
-	double MAX_LATENCY_PER_SERVER = 1000;
+	// never used
+	//double MAX_LATENCY_PER_SERVER = 1000;
 
 	int MAX_SERVER_COUNT_PER_LOC = 5;
 	
@@ -25,12 +28,83 @@ public class Scenario {
 	// A B C D
 	double[] cost = {.5,.7,1,.5};
 	double[] power = {150,200,300,80};
-	double[] powerPerNormal = {150.0/50.0, 200.0/130.0, 300.0/150.0};
+	double[] powerPerNormal = {150.0/50.0, 200.0/130.0, 300.0/150.0, 80.0 / 25.0};
 	long[] latency = {120,120,120,60};
 	
 	// constants for each tactic
 	// DD, DT, ID, IT, ShS, StS
 	double[] failChance = {0.05,0.01,0.05,0.05,0.1,0.1};
 	long[] executionTime = {1,5,1,5,30};
+	
+	interface TacticHandler {
+		void acceptTactic(Tactic tactic);
+	}
+	
+	List<TacticHandler> tacticHandlers;
+	
+	public Scenario() {
+		tacticHandlers = new ArrayList<TacticHandler>();
+	}
+	
+	public void applyTo(Omnet omnet) {
+		omnet.SYSTEM_DEMAND = SYSTEM_DEMAND;
+		Omnet.NORMAL_PROFIT_PER_SECOND = NORMAL_PROFIT_PER_SECOND;
+		Omnet.DIMMED_PROFIT_PER_SECOND = DIMMED_PROFIT_PER_SECOND;
+		
+		if (fourservEnabled) {
+			omnet.getDatacenters().add((omnet.datacenterFactory.getD()));
+		}
+	}
+	
+	public int getSYSTEM_DEMAND() {
+		return SYSTEM_DEMAND;
+	}
+
+	public double getNORMAL_PROFIT_PER_SECOND() {
+		return NORMAL_PROFIT_PER_SECOND;
+	}
+
+	public double getDIMMED_PROFIT_PER_SECOND() {
+		return DIMMED_PROFIT_PER_SECOND;
+	}
+
+	public int getMAX_SERVER_COUNT_PER_LOC() {
+		return MAX_SERVER_COUNT_PER_LOC;
+	}
+
+	public boolean isFourservEnabled() {
+		return fourservEnabled;
+	}
+
+	public double[] getCost() {
+		return cost;
+	}
+
+	public double[] getPower() {
+		return power;
+	}
+
+	public double[] getPowerPerNormal() {
+		return powerPerNormal;
+	}
+
+	public long[] getLatency() {
+		return latency;
+	}
+
+	public double[] getFailChance() {
+		return failChance;
+	}
+
+	public long[] getExecutionTime() {
+		return executionTime;
+	}
+
+	// allow for arbitrary modifications to tactics
+	public void acceptTactic(Tactic tactic) {
+		for (TacticHandler f : tacticHandlers) {
+			f.acceptTactic(tactic);
+		}
+	}
 
 }
