fe63fd22b63aec7d6885f60977e1d9d80db8d8c8
Cody Kinneer <ckinneer@cs.cmu.edu>
2019-09-03 13:25:33 -0400

    refactor how tactic failures are handled for easier output / setting

--- src/main/java/omnet/Scenario.java
+++ src/main/java/omnet/Scenario.java
@@ -1,15 +1,25 @@
 package omnet;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
+import omnet.Scenario.TacticHandler;
 import omnet.components.Datacenter;
 import omnet.components.DatacenterFactory;
+import omnet.tactics.DecreaseDimmer;
+import omnet.tactics.DecreaseTraffic;
+import omnet.tactics.IncreaseDimmer;
+import omnet.tactics.IncreaseTraffic;
+import omnet.tactics.ShutdownServer;
+import omnet.tactics.StartServer;
 import tactics.FailableTactic;
 import tactics.Tactic;
 
 public class Scenario {
 
+	private static Class[] tacticClasses = {DecreaseDimmer.class, DecreaseTraffic.class, IncreaseDimmer.class, IncreaseTraffic.class, ShutdownServer.class, StartServer.class};
+	
 	// omnet constant parameters
 	int SYSTEM_DEMAND = 1000;
 	
@@ -32,9 +42,11 @@ public class Scenario {
 	double[] powerPerNormal = {150.0/50.0, 200.0/130.0, 300.0/150.0, 80.0 / 25.0};
 	long[] latency = {120,120,120,60};
 	
-	// constants for each tactic, note that these aren't applied yet
+	// constants for each tactic
 	// DD, DT, ID, IT, ShS, StS
-	//double[] failChance = {0.05,0.01,0.05,0.05,0.1,0.1};
+	double[] failChance = {0.05,0.01,0.05,0.05,0.1,0.1};
+	
+	// note that this execution time isnt set yet
 	//long[] executionTime = {1,5,1,5,30};
 	
 	interface TacticHandler {
@@ -43,11 +55,26 @@ public class Scenario {
 	
 	List<TacticHandler> tacticHandlers;
 	
+	HashMap<Class,TacticHandler> tacticFailrateHandlers;
+	
 	public Scenario() {
 		tacticHandlers = new ArrayList<TacticHandler>();
+		tacticFailrateHandlers = new HashMap<Class,TacticHandler>();
+		// initialize handlers w/ defaults
+		updateHandlers();
+	}
+	
+	// warning, currently this overwrites all of the existing tactic handlers
+	public void updateHandlers() {
+		for (int count = 0; count < tacticClasses.length; count++) {
+			tacticFailrateHandlers.put(tacticClasses[count], getFailchanceHandler(tacticClasses[count],failChance[count]));
+		}
+		tacticHandlers = new ArrayList<TacticHandler>();
+		tacticHandlers.addAll(tacticFailrateHandlers.values());
 	}
 	
 	public void applyTo(Omnet omnet) {
+		updateHandlers();
 		omnet.SYSTEM_DEMAND = SYSTEM_DEMAND;
 		Omnet.NORMAL_PROFIT_PER_SECOND = NORMAL_PROFIT_PER_SECOND;
 		Omnet.DIMMED_PROFIT_PER_SECOND = DIMMED_PROFIT_PER_SECOND;
@@ -93,9 +120,9 @@ public class Scenario {
 		return latency;
 	}
 
-//	public double[] getFailChance() {
-//		return failChance;
-//	}
+	public double[] getFailChance() {
+		return failChance;
+	}
 //
 //	public long[] getExecutionTime() {
 //		return executionTime;
@@ -106,6 +133,47 @@ public class Scenario {
 		for (TacticHandler f : tacticHandlers) {
 			f.acceptTactic(tactic);
 		}
+	}
+	
+	// add a handler to set the tactic failrate
+	private static TacticHandler getFailchanceHandler(Class c, double failchance) {
+		return (tactic)->{
+			if (tactic instanceof FailableTactic) {
+				FailableTactic ft = (FailableTactic) tactic;
+				if (c.isInstance(tactic)){
+					StartServer s = (StartServer) tactic;
+					ft.setFailChance(failchance);
+				}
+			}
+		};
+	}
+	
+	public String toString() {
+		String ans = SYSTEM_DEMAND + "," + fourservEnabled + ",";
+		ans += arrayToString(toObjectArray(cost)) + ",";
+		ans += arrayToString(toObjectArray(power)) + ",";
+		ans += arrayToString(toObjectArray(powerPerNormal)) + ",";
+		ans += arrayToString(toObjectArray(failChance));
+		return ans;
+	}
+	
+	private static Double[] toObjectArray(double[] array) {
+		Double[] ans = new Double[array.length];
+		for (int i = 0; i < array.length; i++) {
+			ans[i] = (Double) array[i];
+		}
+		return ans;
+	}
+	
+	private <T> String arrayToString(T[] array) {
+		String ans = "";
+		for (int i = 0; i < array.length; i++) {
+			if (i > 0) {
+				ans += ",";
+			}
+			ans += array[i].toString();
+		}
+		return ans;
 	}
 
 }
