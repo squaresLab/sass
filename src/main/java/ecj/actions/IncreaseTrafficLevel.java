b6a0e7d37962e355d51c906cd6be815ad0fcec19
Cody Kinneer <ckinneer@cs.cmu.edu>
2019-10-28 16:35:49 -0400

    fixed bug in javagen for IncreaseTrafficLevel tactic

--- src/main/java/ecj/actions/IncreaseTrafficLevel.java
+++ src/main/java/ecj/actions/IncreaseTrafficLevel.java
@@ -8,10 +8,5 @@ public class IncreaseTrafficLevel extends LabeledTactic {
 	public IncreaseTrafficLevel(){ 
 		super(IncreaseTraffic.class);
 	}
-	
-	public JavaRep generateJava(JavaRep java) {
-		java.appendLine("", this);
-		return java;
-	}
 
 }
