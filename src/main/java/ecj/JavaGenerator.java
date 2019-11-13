ad5e6b363ba5bad4782d1879dbbf1e1f28e71819
Cody Kinneer <ckinneer@cs.cmu.edu>
2019-11-13 16:28:03 -0500

    added a new generateVector method to java generator interface for diy deckard

--- src/main/java/ecj/JavaGenerator.java
+++ src/main/java/ecj/JavaGenerator.java
@@ -1,11 +1,13 @@
 package ecj;
 
 import java.io.Serializable;
+import java.util.HashMap;
 
 import ec.gp.GPNode;
 
 public interface JavaGenerator extends Serializable {
 	
 	public JavaRep generateJava(JavaRep java);
+	public HashMap<String,Integer> generateVector();
 
 }
