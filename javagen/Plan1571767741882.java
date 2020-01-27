b6c5bb2e0eef84456110eeb077ff257325b4b0fc
Cody Kinneer <ckinneer@cs.cmu.edu>
2020-01-27 12:14:32 -0500

    clean up old generated content, vectors, serialized plans, java reps, clusters

--- javagen/Plan1571767741882.java
+++
@@ -1,7 +0,0 @@
-public class Plan1571767741882 extends Plan { 
-public static void main(String[] args) { 
-IncreaseDimmer("B");
-DecreaseTraffic("C");
-
-}
-}
--- javagen/Plan1571772977839.java
+++
@@ -1,8 +0,0 @@
-public class Plan1571772977839 extends Plan { 
-public static void main(String[] args) { 
-for (int i = 0; i < 2 ; i++) {
-IncreaseTraffic("A");
-}
-
-}
-}
