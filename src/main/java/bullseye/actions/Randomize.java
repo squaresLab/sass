f9e2da8088f228b77c9d3d60e70ceea49101daa7
Cody Kinneer <ckinneer@cs.cmu.edu>
2020-11-04 18:17:36 -0500

    added test ecj nodes for mixed strategy support

---
+++ src/main/java/bullseye/actions/Randomize.java
@@ -0,0 +1,24 @@
+package bullseye.actions;
+
+import ec.EvolutionState;
+import ec.Problem;
+import ec.gp.ADFStack;
+import ec.gp.GPData;
+import ec.gp.GPIndividual;
+import ec.gp.GPNode;
+
+public class Randomize extends GPNode {
+
+	@Override
+	public void eval(EvolutionState arg0, int arg1, GPData arg2, ADFStack arg3, GPIndividual arg4, Problem arg5) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
